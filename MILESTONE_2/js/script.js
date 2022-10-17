e45429e4cb09dde0248660e0d5dfe5e7489cbea2
DenisConsta <denisconstantin338@gmail.com>
2022-10-17 13:41:00 +0200

    [add] milestone #2

--- MILESTONE_2/js/script.js
+++ MILESTONE_2/js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+
+const images = [
+  '01.jpg',
+  '02.jpg',
+  '03.jpg',
+  '04.jpg',
+  '05.jpg'
+];
+
+const slider = document.querySelector('.img-container');
+let imgTags = '';
+
+for(let i=0; i<images.length; i++){
+  imgTags += `
+    <img class="item " src="assets/img/${images[i]}" alt="">
+  `;
+}
+
+const items = document.getElementsByClassName('item');
+slider.innerHTML += imgTags;
+console.log(items);
+
+items[0].classList.add('d-block');
